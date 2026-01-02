17f45931d98d6faa23014a318ef37e052b156e3a
Juahjoah <07juah21@gmail.com>
2026-01-02 21:29:44 +0900

    feat: nav init

---
+++ first_moon_pic/components/navigation/CategoryNav.tsx
@@ -0,0 +1,11 @@
+const CategoryNav = () => {
+    return (
+        <nav className="flex space-x-4 p-4 bg-gray-100 dark:bg-gray-900">
+            <a href="#" className="text-gray-700 dark:text-gray-300 hover:underline">Category 1</a>
+            <a href="#" className="text-gray-700 dark:text-gray-300 hover:underline">Category 2</a>
+            <a href="#" className="text-gray-700 dark:text-gray-300 hover:underline">Category 3</a>
+        </nav>
+    )
+}
+
+export default CategoryNav;
