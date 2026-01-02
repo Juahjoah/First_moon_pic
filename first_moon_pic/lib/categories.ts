1d92e2254de5871aef64fdfa7526e2f9fa249bb6
Juahjoah <07juah21@gmail.com>
2026-01-02 23:38:36 +0900

    docs: 사진 카테고리 추가

---
+++ first_moon_pic/lib/categories.ts
@@ -0,0 +1,17 @@
+export const categories = [
+  {
+    id: "fashion",
+    label: "Fashion",
+    description: "패션 사진",
+  },
+  {
+    id: "fine-art",
+    label: "Fine Art",
+    description: "개인 작업 및 순수 사진",
+  },
+  {
+    id: "commercial",
+    label: "Commercial",
+    description: "제품 및 광고 촬영",
+  },
+];
