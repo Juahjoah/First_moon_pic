333414792853299e2ffb8137bb3e3903ffc54fa9
Juahjoah <07juah21@gmail.com>
2026-01-02 21:29:29 +0900

    feat: header init

---
+++ first_moon_pic/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="flex items-center justify-between p-4 bg-white dark:bg-black border-b border-gray-200 dark:border-gray-800">
+            <h1 className="text-xl font-bold">First Moon Pic</h1>
+        </header>
+    )
+}
+
+export default Header;
