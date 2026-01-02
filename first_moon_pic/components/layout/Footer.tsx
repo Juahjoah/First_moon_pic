33904c0151045d0c02716a159add057f974b0fdc
Juahjoah <07juah21@gmail.com>
2026-01-02 21:29:18 +0900

    feat: footer init

---
+++ first_moon_pic/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="flex items-center justify-center p-4 bg-white dark:bg-black border-t border-gray-200 dark:border-gray-800">
+            <p className="text-sm text-gray-500 dark:text-gray-400">© 2024 First Moon Pic. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer;
