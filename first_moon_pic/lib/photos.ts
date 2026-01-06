dfb8eeebd14ba4f61627bdf06e17c50f0ab1f943
Juahjoah <07juah21@gmail.com>
2026-01-07 00:00:03 +0900

    docs: 사진 구조 정리

---
+++ first_moon_pic/lib/photos.ts
@@ -0,0 +1,19 @@
+export const photos = {
+  fashion: [
+    {
+      src: "/images/fashion/fashion-001.jpg",
+      title: "Black dress",
+      description: "full body shot",
+    },
+    {
+      src: "/images/fashion/fashion-002.jpg",
+      title: "Black dress 02",
+      description: "full body shot",
+    },
+    {
+      src: "/images/fashion/fashion-003.jpg",
+      title: "Black dress 03",
+      description: "side shot",
+    },
+  ],
+};
