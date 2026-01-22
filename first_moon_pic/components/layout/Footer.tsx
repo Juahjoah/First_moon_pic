f3012b54a2a3baaf24e3171d59b561c4afb788b3
Juahjoah <07juah21@gmail.com>
2026-01-22 23:27:21 +0900

    feat: 풋터 작성

--- first_moon_pic/components/layout/Footer.tsx
+++ first_moon_pic/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="flex items-center justify-center p-4 bg-white dark:bg-black border-t border-gray-200 dark:border-gray-800">
+        <footer className="flex items-center justify-center p-4">
             <p className="text-sm text-gray-500 dark:text-gray-400">© 2024 First Moon Pic. All rights reserved.</p>
         </footer>
     )
