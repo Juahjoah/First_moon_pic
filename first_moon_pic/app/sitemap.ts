b6b48950fdbe9b41aeea711a2fc1c5beda22ae30
Juahjoah <07juah21@gmail.com>
2026-03-24 23:38:31 +0900

    docs: sitemap.ts 수정

--- first_moon_pic/app/sitemap.ts
+++ first_moon_pic/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { photos } from "@/lib/photos";
 
 export default function sitemap() {
-  const baseUrl = "https://https://www.firstmoonpic.co.kr";
+  const baseUrl = "https://www.firstmoonpic.co.kr";
 
   const allPhotos = Object.values(photos).flat();
 
@@ -9,13 +9,7 @@ export default function sitemap() {
     {
       url: baseUrl,
       lastModified: new Date(),
-
-      // 👇 이게 핵심 (이미지 SEO)
-      images: allPhotos.map((photo) => ({
-        url: `${baseUrl}${photo.src}`, // 중요
-        title: photo.title,
-        caption: photo.description,
-      })),
+      images: allPhotos.map((photo) => `${baseUrl}${photo.src}`),
     },
   ];
 }
